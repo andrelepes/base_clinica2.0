efb13824bfab0ae8a9c9057aae41f06dd1b3c950
andrelepes <andrelepes@hotmail.com>
2023-08-27 12:02:16 -0300

    Update clinicasRoutes.js

--- backend/routes/clinicasRoutes.js
+++ backend/routes/clinicasRoutes.js
@@ -2,59 +2,24 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../database');
 const auth = require('../../authMiddleware');
+const clinicasController = require('../controllers/clinicasController');
 
 // GET para listar todas as clínicas
-router.get('/', auth, async (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem listar clínicas' });
-    }
-    // Lógica para listar clínicas
-});
+router.get('/', auth, clinicasController.listClinicas);
 
 // POST para adicionar uma nova clínica
-router.post('/', auth, (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem adicionar clínicas' });
-    }
-    // Lógica para adicionar clínica
-});
+router.post('/', auth, clinicasController.addClinica);
 
 // PUT para atualizar uma clínica
-router.put('/:id', auth, (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem atualizar clínicas' });
-    }
-    // Lógica para atualizar clínica
-});
+router.put('/:id', auth, clinicasController.updateClinica);
 
 // DELETE para remover uma clínica
-router.delete('/:id', auth, (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem remover clínicas' });
-    }
-    // Lógica para remover clínica
-});
+router.delete('/:id', auth, clinicasController.deleteClinica);
 
 // POST para adicionar um psicólogo vinculado
-router.post('/add-linked-psychologist', auth, (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem adicionar psicólogos vinculados' });
-    }
-    // Lógica para adicionar psicólogo vinculado
-});
+router.post('/add-linked-psychologist', auth, clinicasController.addVinculados);
 
 // POST para adicionar um secretário vinculado
-router.post('/add-linked-secretary', auth, (req, res) => {
-    // Verifique se o usuário é do tipo 'clínica'
-    if (req.user.tipoUsuario !== 'Clinica') {
-        return res.status(403).json({ msg: 'Apenas clínicas podem adicionar secretários vinculados' });
-    }
-    // Lógica para adicionar secretário vinculado
-});
+router.post('/add-linked-secretary', auth, clinicasController.addVinculados);
 
 module.exports = router;
