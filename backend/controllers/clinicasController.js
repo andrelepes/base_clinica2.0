d8edafff76d6eac31dae064d1cf33f4f78ff794a
andrelepes <andrelepes@hotmail.com>
2023-08-27 10:35:49 -0300

    Create clinicasController.js

---
+++ backend/controllers/clinicasController.js
@@ -0,0 +1,31 @@
+const db = require('../database');
+
+// Função para listar todas as clínicas
+exports.listClinicas = async (req, res) => {
+    // Implemente a lógica para listar todas as clínicas
+};
+
+// Função para adicionar uma nova clínica
+exports.addClinica = async (req, res) => {
+    // Implemente a lógica para adicionar uma nova clínica
+};
+
+// Função para atualizar uma clínica existente
+exports.updateClinica = async (req, res) => {
+    // Implemente a lógica para atualizar uma clínica existente
+};
+
+// Função para remover uma clínica
+exports.deleteClinica = async (req, res) => {
+    // Implemente a lógica para remover uma clínica
+};
+
+// Função para adicionar um psicólogo vinculado
+exports.addLinkedPsychologist = async (req, res) => {
+    // Implemente a lógica para adicionar um psicólogo vinculado
+};
+
+// Função para adicionar um secretário vinculado
+exports.addLinkedSecretary = async (req, res) => {
+    // Implemente a lógica para adicionar um secretário vinculado
+};
