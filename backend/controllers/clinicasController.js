b946a320e7d36fc39b6dc5c074842369adc7422e
andrelepes <andrelepes@hotmail.com>
2023-08-28 07:46:42 -0300

    1

--- backend/controllers/clinicasController.js
+++ backend/controllers/clinicasController.js
@@ -2,20 +2,50 @@ const Clinica = require('../models/Clinica');
 
 // Adicionar uma nova clínica
 exports.addClinica = async (req, res) => {
-  // Implementar lógica para adicionar uma nova clínica
+  try {
+    const newClinica = await Clinica.add(req.body);
+    res.status(201).json(newClinica);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Atualizar informações da clínica
 exports.updateClinica = async (req, res) => {
-  // Implementar lógica para atualizar uma clínica existente
+  try {
+    await Clinica.update(req.params.id, req.body);
+    res.status(200).json({ message: 'Clínica atualizada com sucesso' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // Deletar uma clínica
 exports.deleteClinica = async (req, res) => {
-  // Implementar lógica para deletar uma clínica
+  try {
+    await Clinica.delete(req.params.id);
+    res.status(200).json({ message: 'Clínica deletada com sucesso' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-// Adicionar psicólogos e secretários vinculados
-exports.addVinculados = async (req, res) => {
-  // Implementar lógica para adicionar psicólogos e secretários vinculados a uma clínica
+// Adicionar um psicólogo vinculado a uma clínica
+exports.addLinkedPsychologist = async (req, res) => {
+  try {
+    await Clinica.addLinkedPsychologist(req.params.clinicaId, req.body.psychologistId);
+    res.status(201).json({ message: 'Psicólogo vinculado com sucesso' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Adicionar um secretário vinculado a uma clínica
+exports.addLinkedSecretary = async (req, res) => {
+  try {
+    await Clinica.addLinkedSecretary(req.params.clinicaId, req.body.secretaryId);
+    res.status(201).json({ message: 'Secretário vinculado com sucesso' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- backend/models/Clinica.js
+++ backend/models/Clinica.js
@@ -1,40 +1,51 @@
-const mongoose = require('mongoose');
+const db = require('../../database'); 
 
-const ClinicaSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: true
-    },
-    tipoUsuario: {
-        type: String,
-        enum: ['Clinica', 'PsicologoAutonomo', 'PsicologoVinculado', 'SecretarioVinculado'],
-        required: true
-    },
-    cpfCnpj: {
-        type: String,
-        required: true
-    },
-    telefone: {
-        type: String,
-        required: true
-    },
-    cep: {
-        type: String,
-        required: true
-    },
-    endereco: {
-        type: String,
-        required: true
-    },
-    psicologosVinculados: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Usuario'
-    }],
-    secretariosVinculados: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Usuario'
-    }]
-    // Outros campos relevantes
-});
+const Clinica = {
+  // Adicionar uma nova clínica
+  add: (clinica) => {
+    const query = `
+      INSERT INTO clinicas(nome, tipoUsuario, cpfCnpj, telefone, cep, endereco)
+      VALUES($1, $2, $3, $4, $5, $6)
+      RETURNING id;
+    `;
+    return db.one(query, [clinica.nome, clinica.tipoUsuario, clinica.cpfCnpj, clinica.telefone, clinica.cep, clinica.endereco]);
+  },
+  
+  // Atualizar uma clínica existente
+  update: (id, clinica) => {
+    const query = `
+      UPDATE clinicas
+      SET nome = $1, tipoUsuario = $2, cpfCnpj = $3, telefone = $4, cep = $5, endereco = $6
+      WHERE id = $7;
+    `;
+    return db.none(query, [clinica.nome, clinica.tipoUsuario, clinica.cpfCnpj, clinica.telefone, clinica.cep, clinica.endereco, id]);
+  },
+  
+  // Deletar uma clínica
+  delete: (id) => {
+    const query = `
+      DELETE FROM clinicas WHERE id = $1;
+    `;
+    return db.none(query, [id]);
+  },
+  
+  // Adicionar um psicólogo vinculado a uma clínica
+  addLinkedPsychologist: (clinicaId, psychologistId) => {
+    const query = `
+      INSERT INTO linked_psychologists(clinica_id, psychologist_id)
+      VALUES($1, $2);
+    `;
+    return db.none(query, [clinicaId, psychologistId]);
+  },
+  
+  // Adicionar um secretário vinculado a uma clínica
+  addLinkedSecretary: (clinicaId, secretaryId) => {
+    const query = `
+      INSERT INTO linked_secretaries(clinica_id, secretary_id)
+      VALUES($1, $2);
+    `;
+    return db.none(query, [clinicaId, secretaryId]);
+  }
+};
 
-module.exports = mongoose.model('Clinica', ClinicaSchema);
+module.exports = Clinica;
