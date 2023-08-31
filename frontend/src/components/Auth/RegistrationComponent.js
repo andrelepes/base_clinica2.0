8ab3ebdca665406030b1c8c8555121a74b74cf71
andrelepes <andrelepes@hotmail.com>
2023-08-31 09:49:02 -0300

    Update RegistrationComponent.js

--- frontend/src/components/Auth/RegistrationComponent.js
+++ frontend/src/components/Auth/RegistrationComponent.js
@@ -6,23 +6,23 @@ const RegistrationComponent = () => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [tipoUsuario, settipoUsuario] = useState(''); // Adicionado
+  const [tipoUsuario, settipoUsuario] = useState('');
+  const [clinicaId, setClinicaId] = useState(''); // Adicionado novamente
 
   const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
       let payload = { nome, email, senha: password, tipoUsuario };
-  
-      // Se o tipo de usuário necessita de um clinicaId, adicione-o ao payload
+
       if (['Psicologo Vinculado', 'Secretario Vinculado'].includes(tipoUsuario)) {
         payload = { ...payload, clinica_id: clinicaId };
       }
-  
+
       const response = await api.post('/usuarios/registrar', payload);
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('username', nome);
-  
+
       alert('Registro bem-sucedido! Você será redirecionado para a página de login.');
       navigate('/login');
     } catch (error) {
@@ -30,12 +30,11 @@ const RegistrationComponent = () => {
       alert('Falha no registro. Por favor, tente novamente.');
     }
   };
-  
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Registro</h1>
-      <select onChange={(e) => settipoUsuario(e.target.value)}>  {/* Atualizado */}
+      <select onChange={(e) => settipoUsuario(e.target.value)}>
         <option value="" disabled selected>Selecione seu perfil</option>
         <option value="Clinica">Clínica</option>
         <option value="Psicologo Autonomo">Psicólogo(a) Autônomo(a)</option>
