8465c86a81a4ba4b95b7117e505da45a1f2d6d62
andrelepes <andrelepes@hotmail.com>
2023-08-31 09:19:00 -0300

    Update RegistrationComponent.js

--- frontend/src/components/Auth/RegistrationComponent.js
+++ frontend/src/components/Auth/RegistrationComponent.js
@@ -6,13 +6,13 @@ const RegistrationComponent = () => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [perfil, setPerfil] = useState(''); // Novo campo Perfil
+  const [tipoUsuario, settipoUsuario] = useState(''); // Adicionado
 
   const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
-      const response = await api.post('/usuarios/registrar', { nome, email, senha: password, perfil, tipoUsuario, clinica_id: clinicaId, clinicaSenha }); // Adicionado perfil
+      const response = await api.post('/usuarios/registrar', { nome, email, senha: password, tipoUsuario }); // Atualizado
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('username', nome);
 
@@ -27,12 +27,10 @@ const RegistrationComponent = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Registro</h1>
-      <select onChange={(e) => setPerfil(e.target.value)}>
+      <select onChange={(e) => settipoUsuario(e.target.value)}>  {/* Atualizado */}
         <option value="" disabled selected>Selecione seu perfil</option>
         <option value="Clinica">Clínica</option>
         <option value="Psicologo Autonomo">Psicólogo(a) Autônomo(a)</option>
-        <option value="Psicologo Vinculado">Psicólogo(a) Vinculado(a)</option>
-        <option value="Secretario Vinculado">Secretário(a) Vinculado(a)</option>
       </select>
       <input type='text' placeholder='Nome' onChange={(e) => setNome(e.target.value)} />
       <input type='text' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
@@ -43,3 +41,4 @@ const RegistrationComponent = () => {
 };
 
 export default RegistrationComponent;
+
