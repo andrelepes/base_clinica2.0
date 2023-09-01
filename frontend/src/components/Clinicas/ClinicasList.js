f26106360e45ec82c29a41ee961461fd99c985ed
andrelepes <andrelepes@hotmail.com>
2023-09-01 14:30:10 -0300

    Update ClinicasList.js

--- frontend/src/components/Clinicas/ClinicasList.js
+++ frontend/src/components/Clinicas/ClinicasList.js
@@ -1,27 +1,26 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import AddClinicaForm from './AddClinicaForm';
 import { ClinicaContext } from '../../contexts/ClinicaContext';
 
 function ClinicasList() {
-  const [clinicas, setClinicas] = useState([]);
-  const { clinicData, setClinicData } = useContext(ClinicaContext);
+  const [setClinicas] = useState([]);
+  const {clinicData} = useContext(ClinicaContext);
   const [showForm, setShowForm] = useState(false);
   const [editingClinica, setEditingClinica] = useState(null);
 
   useEffect(() => {
+    const fetchClinicas = async () => {
+      try {
+        const response = await api.get('/clinicas');
+        setClinicas(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar as clínicas:', error);
+      }
+    };
+
     fetchClinicas();
   }, []);
-
-  const fetchClinicas = async () => {
-    try {
-      const response = await api.get('/clinicas');
-      setClinicas(response.data);
-    } catch (error) {
-      console.error('Erro ao buscar as clínicas:', error);
-    }
-  };
 
   const handleNewClinica = async (clinicaData) => {
     try {
