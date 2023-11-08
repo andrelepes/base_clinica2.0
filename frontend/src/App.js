a98d070d9456b93eb08f74f72ae3507c949a0d76
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2023-11-08 18:29:02 -0300

    Refactor Login Page and delete template test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import PacientesList from './components/Pacientes/PacientesList';
 import PacienteDetalhes from './components/Pacientes/PacienteDetalhes';
 import Agenda from './components/Agenda/Agenda';
-import LoginComponent from './components/Auth/LoginComponent';
 import RegistrationComponent from './components/Auth/RegistrationComponent';
 import ClinicasList from './components/Clinicas/ClinicasList';
 import { ClinicaProvider } from './contexts/ClinicaContext'; // Import ClinicaProvider
 import { ClinicaIdProvider } from './contexts/ClinicaIdContext'; // Import ClinicaIdProvider
+import Login from './pages/Login';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { Theme } from './styles/theme';
 
 function HomePage() {
-    return (
-        <header className='App-header'>
-            Selecione uma opção na barra lateral para exibir o conteúdo aqui.
-        </header>
-    );
+  return (
+    <header className="App-header">
+      Selecione uma opção na barra lateral para exibir o conteúdo aqui.
+    </header>
+  );
 }
 
 function App() {
@@ -40,36 +48,46 @@ function App() {
   }, []);
 
   return (
-    <ClinicaIdProvider> {/* Wrap the components with ClinicaIdProvider */}
-      <ClinicaProvider> {/* Wrap the components with ClinicaProvider */}
-        <Router>
-          <div className='App'>
-            {token ? (
-              <>
-                <div className='sidebar'>
-                  <Sidebar />
-                </div>
-                <div className='main-content'>
-                  <Routes>
-                    <Route path='/pacientes/:id' element={<PacienteDetalhes />} />
-                    <Route path='/pacientes' element={<PacientesList />} />
-                    <Route path='/agenda' element={<Agenda />} />
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='/clinicas' element={<ClinicasList />} />
-                  </Routes>
-                </div>
-              </>
-            ) : (
-              <Routes>
-                <Route path='/login' element={<LoginComponent />} />
-                <Route path='/register' element={<RegistrationComponent />} />
-                <Route path='*' element={<Navigate to='/login' />} />
-              </Routes>
-            )}
-          </div>
-        </Router>
-      </ClinicaProvider>
-    </ClinicaIdProvider>
+    <ThemeProvider theme={Theme}>
+      <CssBaseline/>
+      <ClinicaIdProvider>
+        {' '}
+        {/* Wrap the components with ClinicaIdProvider */}
+        <ClinicaProvider>
+          {' '}
+          {/* Wrap the components with ClinicaProvider */}
+          <Router>
+            <div className="App">
+              {token ? (
+                <>
+                  <div className="sidebar">
+                    <Sidebar />
+                  </div>
+                  <div className="main-content">
+                    <Routes>
+                      <Route
+                        path="/pacientes/:id"
+                        element={<PacienteDetalhes />}
+                      />
+                      <Route path="/pacientes" element={<PacientesList />} />
+                      <Route path="/agenda" element={<Agenda />} />
+                      <Route path="/" element={<HomePage />} />
+                      <Route path="/clinicas" element={<ClinicasList />} />
+                    </Routes>
+                  </div>
+                </>
+              ) : (
+                <Routes>
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<RegistrationComponent />} />
+                  <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+              )}
+            </div>
+          </Router>
+        </ClinicaProvider>
+      </ClinicaIdProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/styles/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const Theme = createTheme({
+  palette: {
+    primary: { main: '#156AED' },
+    principal: { main: '#1FD0E0' },
+    middleBlue: { main: '#16A9F7' },
+    middleGreen: { main: '#16F7CC' },
+    success: { main: '#15ED86' },
+  },
+  shape: {
+    borderRadius: 6,
+  },
+});
